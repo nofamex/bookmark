a455869ca7eaf3d20fd24074d9e344c6697c51f2
nofaldi atmam <nofaldi.atmam@shopee.com>
2023-09-02 18:28:44 +0700

    feat: add form validation

--- src/components/module/entry.tsx
+++ src/components/module/entry.tsx
@@ -2,7 +2,6 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useBookmark } from "@/lib/store";
-import { useState } from "react";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -14,23 +13,47 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { nanoid } from "nanoid";
+import { useFormik } from "formik";
+import { z } from "zod";
+import { toFormikValidationSchema } from "zod-formik-adapter";
+import { useState } from "react";
+
+const schema = z.object({
+  title: z.string(),
+  url: z.string().url(),
+});
 
 const Entry = () => {
   const addNewBookmark = useBookmark((state) => state.add);
-  const [title, setTitle] = useState<string>("");
-  const [url, setUrl] = useState<string>("");
 
-  const handleSubmit = () => {
-    if (title === "" || url === "") return;
-    addNewBookmark({ id: nanoid(), title, url });
-    setTitle("");
-    setUrl("");
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      url: "",
+    },
+    validationSchema: toFormikValidationSchema(schema),
+    onSubmit: (values) => {
+      if (formik.errors) {
+      }
+      addNewBookmark({ id: nanoid(), title: values.title, url: values.url });
+      formik.resetForm();
+      setIsOpen(!isOpen);
+    },
+  });
+
+  const handleClose = () => {
+    formik.resetForm();
+    setIsOpen(!isOpen);
   };
 
   return (
-    <AlertDialog>
+    <AlertDialog open={isOpen}>
       <AlertDialogTrigger asChild>
-        <Button className="w-full h-2/3">Add Bookmark</Button>
+        <Button className="w-full h-2/3" onClick={() => setIsOpen(!isOpen)}>
+          Add Bookmark
+        </Button>
       </AlertDialogTrigger>
       <AlertDialogContent className="sm:max-w-[425px]">
         <AlertDialogHeader>
@@ -38,35 +61,39 @@ const Entry = () => {
         </AlertDialogHeader>
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="bookmark-title" className="text-right font-bold">
+            <Label htmlFor="title" className="text-right font-bold">
               Title
             </Label>
             <Input
               type="text"
-              id="bookmark-title"
-              placeholder="bookmark title"
+              id="title"
+              name="title"
+              placeholder="my bookmark"
               className="col-span-3"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
+              value={formik.values.title}
+              onChange={formik.handleChange}
             />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="bookmark-url" className="text-right font-bold">
+            <Label htmlFor="url" className="text-right font-bold">
               Url
             </Label>
             <Input
               type="url"
-              id="bookmark-url"
-              placeholder="bookmark url"
+              id="url"
+              name="url"
+              placeholder="https://bookmark.com"
               className="col-span-3"
-              value={url}
-              onChange={(e) => setUrl(e.target.value)}
+              value={formik.values.url}
+              onChange={formik.handleChange}
             />
           </div>
         </div>
         <AlertDialogFooter className="flex-row items-center justify-end">
-          <AlertDialogCancel className="mt-0 mr-2">Cancel</AlertDialogCancel>
-          <AlertDialogAction onClick={() => handleSubmit()}>
+          <AlertDialogCancel className="mt-0 mr-2" onClick={handleClose}>
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction onClick={() => formik.handleSubmit()}>
             Continue
           </AlertDialogAction>
         </AlertDialogFooter>
